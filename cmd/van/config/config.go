c6d0bf5458bf792f601383a01f7bbcf3a80f593e
moocss <moocss@gmail.com>
2024-08-19 11:11:25 +0800

    add: auth, subscriptions, template

---
+++ cmd/van/config/config.go
@@ -0,0 +1,9 @@
+package main
+
+const (
+	Release      = "v0.2.0"
+	VanCmd       = "github.com/apus-run/van@latest"
+	RepoBase     = "github.com/apus-run/van-layout-starter/van-layout-basic.git"
+	RepoAdvanced = "github.com/apus-run/van-layout-starter/van-layout-advanced.git"
+	RepoMultiple = "github.com/apus-run/van-layout-starter/van-layout-multiple.git"
+)
